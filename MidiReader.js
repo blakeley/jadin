b62348fa0c5c9634a95f4c1aec1a002fd237d36a
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-01-24 19:40:31 -0800

    Add MidiReader#read

--- MidiReader.js
+++ MidiReader.js
@@ -1,5 +1,13 @@
 function MidiReader(data) {
   this.data = data;
+  this.position = 0;
 };
+
+MidiReader.prototype.read = function(length) {
+  var result = this.data.substr(this.position, length);
+  this.position += length;
+  return result;
+}
+
 
 module.exports = MidiReader;
--- test/MidiReader.js
+++ test/MidiReader.js
@@ -2,10 +2,14 @@ var MidiReader = require('../MidiReader');
 var expect = require('chai').expect;
 var fs = require('fs');
 
-var cScaleData = fs.readFileSync('fixtures/c.mid');
+var cScaleData = fs.readFileSync('fixtures/c.mid', 'utf8');
 var cScaleMidiReader = new MidiReader(cScaleData);
 
 it('should construct a MidiReader instance', function(){
   expect(cScaleMidiReader).to.not.be.null();
 });
 
+it('#read should return the requested number of bytes', function(){
+  result = cScaleMidiReader.read(4);
+  expect(result).to.equal('MThd');
+});
