2bac181aa6e5c06cae2c66b4cac6ffd97cf07f7b
Brandon Blakeley <brandon.blakeley@gmail.com>
2015-02-09 16:08:50 -0800

    Parse notes and events in Track constructor

--- Track.js
+++ Track.js
@@ -2,7 +2,33 @@ var MidiReader = require('./MidiReader');
 
 
 function Track(data) {
+  this.events = [];
+  this.notes = [];
+
   var reader = new MidiReader(data);
+  var noteOnEvents = {}
+  var currentTick = 0;
+  while (!reader.isAtEndOfFile()) {
+    var event = reader.readEvent();
+    currentTick += event.deltaTime;
+    event.tick  = currentTick;
+    this.events.push(event);
+
+    switch(event.subtype){
+      case 'noteOn':
+        noteOnEvents[event.pitch] = event;
+        break;
+      case 'noteOff':
+        if (noteOnEvents[event.pitch] === undefined) throw "noteOff event without corresponding noteOn event";
+        var noteOnEvent = noteOnEvents[event.pitch];
+        note = {
+          pitch: event.pitch
+        };
+        this.notes.push(note);
+        break;
+    }
+  }
+
 
 }
 
--- test/Track.js
+++ test/Track.js
@@ -5,14 +5,25 @@ var expect = require('chai').expect;
 
 var cScaleData = fs.readFileSync('fixtures/c.mid', 'binary');
 var reader = new MidiReader(cScaleData);
-var headerData = reader.readChunk();
-var track1Data = reader.readChunk();
-var track2Data = reader.readChunk();
-var track3Data = reader.readChunk();
-var track2 = new Track(track2Data);
+var headerChunk = reader.readChunk();
+var track1Chunk = reader.readChunk();
+var track2Chunk = reader.readChunk();
+var track3Chunk = reader.readChunk();
+var track2 = new Track(track2Chunk.data);
 
 describe('Track', function(){
   it('should construct a Track instance', function(){
     expect(track2).to.not.be.null()
   });
+
+  it('#events should return an array of all events', function(){
+    expect(track2.events).to.not.be.undefined();
+    expect(track2.events.length).to.equal(11);    
+  });
+
+  it('#notes should return an array of all notes', function(){
+    expect(track2.notes).to.not.be.undefined();
+    expect(track2.notes.length).to.equal(4);
+  });
+
 })
