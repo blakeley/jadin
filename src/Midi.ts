c4c644049c86740c6f117c9b1bdbbba59e43e71f
Brandon Blakeley <brandon.blakeley@gmail.com>
2020-04-04 19:04:01 -0500

    Move Midi instance variables instantiations into inside constructor

--- src/Midi.ts
+++ src/Midi.ts
@@ -1,24 +1,30 @@
-import MidiReader from './MidiReader';
-import Track from './Track';
-import Cursor from './Cursor';
-import Event from './Event';
-import Note from './Note';
+import MidiReader from "./MidiReader";
+import Track from "./Track";
+import Cursor from "./Cursor";
+import Event from "./Event";
+import Note from "./Note";
 
 export default class Midi {
-  format = 0;
-  ppqn = 480;
-  tracks: Track[] = [];
-  private _tickToSecond: {[key: number]: number} = {};
-  private _tempoEvents: Event[] = [];
+  format: number;
+  ppqn: number;
+  tracks: Track[];
+  private _tickToSecond: { [key: number]: number };
+  private _tempoEvents: Event[];
 
   constructor(data?: string) {
-    if(!!data){
+    this.format = 0;
+    this.ppqn = 480;
+    this.tracks = [];
+    this._tickToSecond = {};
+    this._tempoEvents = [];
+
+    if (!!data) {
       const reader = new MidiReader(data);
 
       const headerChunk = reader.readChunk();
       const headerReader = new MidiReader(headerChunk.data);
       this.format = headerReader.readInt(2);
-      if(this.format == 2) throw "MIDI format 2 not supported";
+      if (this.format == 2) throw "MIDI format 2 not supported";
       const numberOfTracks = headerReader.readInt(2);
       this.ppqn = headerReader.readInt(2); // assumes metrical timing
 
@@ -39,62 +45,86 @@ export default class Midi {
   }
 
   newCursor() {
-    return new Cursor(this.events.sort(function(e1,e2){return e1.tick! - e2.tick!}));
+    return new Cursor(
+      this.events.sort(function (e1, e2) {
+        return e1.tick! - e2.tick!;
+      })
+    );
   }
 
   get notes() {
     return this.tracks
-      .map(function(track){return track.notes})
-      .reduce(function(a,b){return a.concat(b)});
+      .map(function (track) {
+        return track.notes;
+      })
+      .reduce(function (a, b) {
+        return a.concat(b);
+      });
   }
 
   get events() {
     return this.tracks
-      .map(function(track){return track.events})
-      .reduce(function(a,b){return a.concat(b)});
-      //.sort(function(e1,e2){return e1.tick < e2.tick});
+      .map(function (track) {
+        return track.events;
+      })
+      .reduce(function (a, b) {
+        return a.concat(b);
+      });
+    //.sort(function(e1,e2){return e1.tick < e2.tick});
   }
 
   get tempoEvents() {
-    if(this._tempoEvents.length > 0) return this._tempoEvents; // return if memoized
+    if (this._tempoEvents.length > 0) return this._tempoEvents; // return if memoized
 
     // format 0: All events are on the zeroth track, including tempo events
     // format 1: All tempo events are on the zeroth track
     // format 2: Every track has tempo events (not supported)
-    return this._tempoEvents = this.tracks[0].events.filter(function(event){
-      return event.subtype == 'setTempo';
-    })
+    return (this._tempoEvents = this.tracks[0].events.filter(function (event) {
+      return event.subtype == "setTempo";
+    }));
   }
 
   get duration() {
     return this.notes
-      .map(function(note){return note.offSecond})
-      .reduce(function(a,b){return Math.max(a!, b!)}, 0)
+      .map(function (note) {
+        return note.offSecond;
+      })
+      .reduce(function (a, b) {
+        return Math.max(a!, b!);
+      }, 0);
   }
 
   tickToSecond(tick: number) {
-    if(this._tickToSecond[tick]) return this._tickToSecond[tick]
+    if (this._tickToSecond[tick]) return this._tickToSecond[tick];
 
     var currentTick = 0;
     var currentTempo = 500000;
     var totalTime = 0;
     for (var i = 0; i < this.tempoEvents.length; i++) {
       var event = this.tempoEvents[i];
-      if(event.tick! >= tick) break;
-      totalTime += ((event.tick! - currentTick) / this.ppqn) * currentTempo / 1000000.0;
+      if (event.tick! >= tick) break;
+      totalTime +=
+        (((event.tick! - currentTick) / this.ppqn) * currentTempo) / 1000000.0;
       currentTick = event.tick!;
       currentTempo = event.tempo!;
     }
 
-    totalTime += ((tick - currentTick) / this.ppqn) * currentTempo / 1000000.0;
-    return this._tickToSecond[tick] = totalTime;
+    totalTime +=
+      (((tick - currentTick) / this.ppqn) * currentTempo) / 1000000.0;
+    return (this._tickToSecond[tick] = totalTime);
   }
 
   notesOnAt(second: number): Note[] {
-    return [].concat(...this.tracks.map(track => track.notesOnAt(second)) as any);
+    return [].concat(
+      ...(this.tracks.map((track) => track.notesOnAt(second)) as any)
+    );
   }
 
   notesOnDuring(onSecond: number, offSecond: number): Note[] {
-    return [].concat(...this.tracks.map(track => track.notesOnDuring(onSecond, offSecond)) as any);
+    return [].concat(
+      ...(this.tracks.map((track) =>
+        track.notesOnDuring(onSecond, offSecond)
+      ) as any)
+    );
   }
 }
